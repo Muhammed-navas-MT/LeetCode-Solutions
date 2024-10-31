19bf6d842b37c3c11ee38061c93ed7858b96dcda
Muhammed Navas <muhammednavas382@gmail.com>
2024-10-31 05:58:44 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 49.2 MB (100.00%)

---
+++ solutions/0088-merge-sorted-array/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    nums1.splice(m,nums1.length);
+    nums2.splice(n,nums2.length);
+    for(let i=0;i<nums2.length;i++){
+        nums1.push(nums2[i]);
+    };
+    nums1.sort((a,b)=>a-b);
+};
+    
