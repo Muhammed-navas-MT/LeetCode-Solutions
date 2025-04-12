7cab249ce9d1db127423d5b602d26cbf670dd34d
Muhammed Navas <muhammednavas382@gmail.com>
2025-04-12 06:19:00 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 54.1 MB (25.81%)

--- solutions/1584-average-salary-excluding-the-minimum-and-maximum-salary/solution.js
+++ solutions/1584-average-salary-excluding-the-minimum-and-maximum-salary/solution.js
@@ -6,12 +6,10 @@ var average = function(salary) {
     let min = Math.min(...salary);
     let max = Math.max(...salary);
     salary = salary.filter(item => item !== max && item !== min);
-    console.log(salary);
     let sum = 0;
     for(let val of salary){
         sum = sum + val;
     };
-    let l = salary.length
-    console.log(l)
-    return sum/l
+
+    return sum/salary.length
 };
