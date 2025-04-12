0ff08064acba1cb50acaeb22a8292425ae2736b5
Muhammed Navas <muhammednavas382@gmail.com>
2025-04-12 06:23:13 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 54.3 MB (67.83%)

--- solutions/1528-kids-with-the-greatest-number-of-candies/solution.js
+++ solutions/1528-kids-with-the-greatest-number-of-candies/solution.js
@@ -4,13 +4,8 @@
  * @return {boolean[]}
  */
 var kidsWithCandies = function(candies, extraCandies) {
-    // let maxCandies = Math.max(...candies);
-    // let result = candies.map(candy => candy + extraCandies >= maxCandies);
-    
-    // return result;
-    let kids = Math.max(...candies)
-    console.log(kids);
     let arr =[]
+    let kids = Math.max(...candies)
     for(let i=0;i<candies.length;i++){
         let sum = candies[i]+extraCandies
         if(sum>=kids){
@@ -21,4 +16,3 @@ var kidsWithCandies = function(candies, extraCandies) {
     }
     return arr
 };
-
