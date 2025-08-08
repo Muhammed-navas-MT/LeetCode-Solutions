b31b618e2dddd6dcd75789f3c949b9da8ff8a49c
Muhammed Navas <muhammednavas382@gmail.com>
2025-08-08 05:21:40 +0000

    [LeetCode] Runtime - 13 ms (5.97%), Memory - 59 MB (5.97%)

---
+++ solutions/2346-largest-3-same-digit-number-in-string/solution.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestGoodInteger = function(num) {
+    let arr =[];
+    let result = [];
+    for(let i=0;i<num.length;i++){
+        if(arr.length === 3){
+            let str = arr.join("");
+            result.push(+str);
+            arr = [];
+        };
+        if(arr.length === 0){
+            arr.push(num[i])
+        }else if(+num[i] == +arr[arr.length-1]){
+            arr.push(num[i])
+        }else{
+            arr = [];
+            arr.push(num[i])
+        }
+        if(arr.length === 3){
+            let str = arr.join("");
+            result.push(+str);
+            arr = [];
+        };
+    };
+    if(result.length === 0){
+        return ""
+    }
+    console.log(result);
+    let max = Math.max(...result);
+    if(max === 0){
+        return "000"
+    }
+    return `${max}`
+    
+};
