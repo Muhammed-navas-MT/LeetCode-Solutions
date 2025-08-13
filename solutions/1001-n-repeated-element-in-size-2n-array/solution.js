7443b1a4c849dc4e8d51a77d4e47c6bb3669ff54
Muhammed Navas <muhammednavas382@gmail.com>
2025-08-13 05:18:55 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 57.1 MB (64.95%)

---
+++ solutions/1001-n-repeated-element-in-size-2n-array/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var repeatedNTimes = function(nums) {
+    let num = 0
+    for(let i=0;i<nums.length;i++){
+       for(let j=i+1;j<nums.length;j++){
+        if(nums[i] === nums[j]){
+            return nums[i]
+        }
+       }
+    };
+    // let keys = Object.keys(num);
+    // console.log(keys);
+    return num
+};
