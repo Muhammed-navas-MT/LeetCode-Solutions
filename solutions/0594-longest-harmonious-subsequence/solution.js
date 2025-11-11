3bf8cb68a323b79c61ac769c6150f84c94d5e009
Muhammed Navas <muhammednavas382@gmail.com>
2025-11-11 16:22:47 +0000

    [LeetCode] Runtime - 23 ms (49.18%), Memory - 65.3 MB (37.38%)

---
+++ solutions/0594-longest-harmonious-subsequence/solution.js
@@ -0,0 +1,16 @@
+var findLHS = function(nums) {
+    const freq = new Map();
+    for (let num of nums) {
+        freq.set(num, (freq.get(num) || 0) + 1);
+    }
+
+    let longest = 0;
+    for (let [num, count] of freq) {
+        if (freq.has(num + 1)) {
+            longest = Math.max(longest, count + freq.get(num + 1));
+        }
+    }
+
+    return longest;
+};
+
