53731936cda90631273f10e56e86abfda4ad7fc3
Muhammed Navas <muhammednavas382@gmail.com>
2026-06-25 14:49:58 +0000

    [LeetCode] Runtime - 7 ms (91.79%), Memory - 57.5 MB (50.07%)

--- solutions/0242-valid-anagram/solution.js
+++ solutions/0242-valid-anagram/solution.js
@@ -4,27 +4,31 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    let countS = {};
-    let countT = {};
-
-    for(let val of s){
-        countS[val] = (countS[val] || 0) + 1;
-    };
-    for(let val of t){
-         countT[val] = (countT[val] || 0) + 1;
-    }
-    for(let val of s){
-        if(countS[val] !== countT[val]){
-            return false;
-        }else{
-            delete countS[val];
-            delete countT[val];
-        }
-    };
-
-    if(Object.keys(countS).length === 0 && Object.keys(countT).length === 0){
-        return true
-    }else{
+    if(s.length !== t.length){
         return false;
     }
+    
+    let obj1 = getObj(s);
+    let obj2 = getObj(t);
+    let keys = Object.keys(obj1);
+    
+    for(let val of keys){
+        if(obj2[val]){
+            if(obj1[val] !== obj2[val]){
+                return false;
+            }
+        }else {
+            return false;
+        }
+    };
+    
+   return true;
+};
+
+function getObj (str){
+    let obj = {};
+    for(let val of str){
+        obj[val] = (obj[val] || 0) + 1;
+    };
+    return obj;
 };
